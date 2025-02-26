6adb5187a35c4c65e7c7ef2b6cf981324f1ccdae
GergesHany <gergeshany179@gmail.com>
2025-02-26 19:12:53 +0200

    test the pre-commit script

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -1,17 +1,16 @@
 package handler
 
 import (
-	"log"
-    "net/http"
 	"Go-Boilerplate/internal/helpers"
+	"log"
+	"net/http"
 )
 
 // LoginCheckHandler responds with "user logged in successfully" when a POST request is made to /login.
 func LoginCheckHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
+	if r.Method != http.MethodPost {
 		helpers.RespondWithError(w, http.StatusMethodNotAllowed, "method not allowed")
-        return
-    }
-    // Proceed with login logic for POST method...
+		return
+	}
 	log.Printf("Login check successful")
 }
